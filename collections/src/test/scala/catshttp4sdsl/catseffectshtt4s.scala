7557f0d0ff825333af27d0ee83a9cc7233f7b866
ValentinShilin <valentin.shilin.de@gmail.com>
2023-03-15 19:42:01 +0100

    streaming

--- collections/src/test/scala/catshttp4sdsl/catseffectshtt4s.scala
+++ collections/src/test/scala/catshttp4sdsl/catseffectshtt4s.scala
@@ -1,63 +1,57 @@
 package catshttp4sdsl
 
 import cats.data.Kleisli
-import cats.effect.*
+import cats.effect._
 import com.comcast.ip4s.{Host, Port}
 import org.http4s.{Http, HttpRoutes, Request, Response}
-import org.http4s.dsl.io.*
+import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.implicits.*
+import org.http4s.implicits._
 import org.http4s.server.Router
 
 object  Restfull {
-  val service: HttpRoutes[IO] =
-    HttpRoutes.of {
-      case GET -> Root / "hello" / name => Ok("dsg")
-    }
-
-    //правильно декомпозировтаь все роуты на кусочки
-    //тоесть правильно писать какой то шлюз
-    //который в зависимости от адреса перенапрявляет на разные адреса
+  val service: HttpRoutes[IO] = HttpRoutes.of {
+    case GET -> Root / "hello" / name => Ok("web service OK")
+  }
 
   val serviceOne: HttpRoutes[IO] =
     HttpRoutes.of {
-      case GET -> Root / "hello1" / name => Ok("dsg1")
+      case GET -> Root / "hello1" / name => Ok(s"web service from $name")
+      case POST -> Root / "hello2" / name => Ok(s"web service from $name")
+
     }
+
   val serviceTwo: HttpRoutes[IO] =
     HttpRoutes.of {
-      case GET -> Root / "hello2" / name => Ok("dsg2")
+      case GET -> Root / "hello2" / name => Ok("web service OK2")
     }
 
-  val router = Router("/" -> serviceOne, "/api" -> serviceTwo)
+  val router  = Router(
+    "/" -> serviceOne,
+    "/api" -> serviceTwo,
+    "/apiroot" -> service)
 
+  val httpApp: Http[IO,IO] = service.orNotFound
 
-
-  val httpApp: Http[IO, IO] = service.orNotFound
   val server = for {
-    //указываем эффект в котором запускаемся
     s <- EmberServerBuilder
       .default[IO]
       .withPort(Port.fromInt(8080).get)
       .withHost(Host.fromString("localhost").get)
-      .withHttpApp(httpApp).build //и билдим сервер, это ресурс
-  } yield s
+      .withHttpApp(httpApp).build
+  } yield  s
 
   val server1 = for {
-    //указываем эффект в котором запускаемся
     s <- EmberServerBuilder
       .default[IO]
       .withPort(Port.fromInt(8080).get)
       .withHost(Host.fromString("localhost").get)
-      .withHttpApp(router.orNotFound).build //и билдим сервер, это ресурс
-  } yield s
+      .withHttpApp(router.orNotFound).build
+  } yield  s
 }
 
 object mainServer extends IOApp.Simple {
-  def run(): IO[Unit] = {
-//    Restfull.server.use(_ => IO.never)
-
-//2.
-    Restfull.server1.use(_ => IO.never)
+  def run() : IO[Unit] = {
+    Restfull.server1.use( _ => IO.never)
   }
-
 }
